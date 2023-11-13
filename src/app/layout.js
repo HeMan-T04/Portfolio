4e3dcf47b7c2717897117c09246de04371e93333
HeMan-T04 <hemantkathuria04@gmail.com>
2023-11-13 09:42:00 +0530

    analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,22 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'HeManT',
-  description: 'Hello Everyone! My Name is Hemant Kathuria. I am a Cyber Security Analyst and a Full Stack Developer. I enjoy playing CTFs, developing, and learning whenever I get a chance. If you want to play a CTF or discuss anything about security, feel free to ping me.',
-}
+  title: "HeManT",
+  description:
+    "Hello Everyone! My Name is Hemant Kathuria. I am a Cyber Security Analyst and a Full Stack Developer. I enjoy playing CTFs, developing, and learning whenever I get a chance. If you want to play a CTF or discuss anything about security, feel free to ping me.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
